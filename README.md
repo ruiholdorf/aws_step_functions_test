f7a67b1e75ed1bd45d58f1618f2b76ab466931c4
Rui Holdorf <rui.holdorf@sage.com>
2019-05-20 11:21:13 -0300

    blank project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aws_step_functions_test
 Teste com Step Functions do AWS
+
+veja: [Building Lambda Functions with C#](https://docs.aws.amazon.com/lambda/latest/dg/dotnet-programming-model.html)
---
+++ aws_step_functions_tes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aws_step_functions_tes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
